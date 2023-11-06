c48f902a37186c97b731b38808d4c82d349626c3
SARUN <dhatchanamoorthiapcse19@jkkmct.edu.in>
2023-11-06 15:16:05 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-// App.js
 import './App.css';
 import LoginPage from './components/Pages/LoginPage';
 import HomePage from './components/Pages/HomePage';
 import Employee from './components/Pages/Employee';
 import Candidate from './components/Pages/Candidate';
 
-import React, { useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 
 function App() {
-
   const storedLoggedIn = localStorage.getItem('loggedIn');
   const [loggedIn, setLoggedIn] = useState(storedLoggedIn === 'true');
   const [jobData, setJobData] = useState([]);
@@ -19,12 +17,10 @@ function App() {
     localStorage.setItem('loggedIn', 'true');
   };
 
-
   const handleLogout = () => {
     setLoggedIn(false);
     localStorage.setItem('loggedIn', 'false');
   };
-
 
   const loadJobData = () => {
     const storedData = localStorage.getItem('jobData');
@@ -33,54 +29,42 @@ function App() {
     }
   };
 
-
   const saveJobData = (data) => {
     localStorage.setItem('jobData', JSON.stringify(data));
     setJobData(data);
   };
 
-
   useEffect(() => {
     loadJobData();
   }, []);
 
-
-
   const handleJobSubmit = (newJob) => {
-    
     const updatedJobData = [...jobData, newJob];
-    
     saveJobData(updatedJobData);
   };
 
-
-
-
   return (
-<Router>
-  <Routes basename="/job-board">
-    <Route
-      path="/job-board"
-      element={!loggedIn ? <LoginPage setLoggedIn={handleLogin} /> : <Navigate to="/home" />}
-    />
-    <Route
-      path="/home"
-      element={loggedIn ? <HomePage onLogout={handleLogout} /> : <Navigate to="/job-board" />}
-    />
-    <Route
-      path="/employee"
-      element={loggedIn ? <Employee onJobSubmit={handleJobSubmit} /> : <Navigate to="/job-board" />}
-    />
-    <Route
-      path="/candidate"
-      element={loggedIn ? <Candidate onJobSubmit={handleJobSubmit} /> : <Navigate to="/job-board" />}
-    />
-
-    <Route index={true} element={<Navigate to="/job-board"/>} />
-
-  </Routes>
-</Router>
-
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={!loggedIn ? <LoginPage setLoggedIn={handleLogin} /> : <Navigate to="/home" />}
+        />
+        <Route
+          path="/home"
+          element={loggedIn ? <HomePage onLogout={handleLogout} /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/employee"
+          element={loggedIn ? <Employee onJobSubmit={handleJobSubmit} /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/candidate"
+          element={loggedIn ? <Candidate onJobSubmit={handleJobSubmit} /> : <Navigate to="/" />}
+        />
+        <Route index element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
