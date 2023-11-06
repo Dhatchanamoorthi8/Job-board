692c44a13695f96994d0e7d7fcbe8b561d0d0abf
SARUN <dhatchanamoorthiapcse19@jkkmct.edu.in>
2023-11-06 18:30:40 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import Employee from './components/Pages/Employee';
 import Candidate from './components/Pages/Candidate';
 
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom';
+
 
 function App() {
   const storedLoggedIn = localStorage.getItem('loggedIn');
@@ -44,31 +45,30 @@ function App() {
   };
 
   return (
-<Router basename="/job-board">
-  <Routes>
-    <Route
-      path="/job-board"
-      element={!loggedIn ? <LoginPage setLoggedIn={handleLogin} /> : <Navigate to="/home" />}
-    />
-    <Route
-      path="/home"
-      element={loggedIn ? <HomePage onLogout={handleLogout} /> : <Navigate to="/job-board" />}
-    />
-    <Route
-      path="/employee"
-      element={loggedIn ? <Employee onJobSubmit={handleJobSubmit} /> : <Navigate to="/job-board" />}
-    />
-    <Route
-      path="/candidate"
-      element={loggedIn ? <Candidate onJobSubmit={handleJobSubmit} /> : <Navigate to="/job-board" />}
-    />
-
-    <Route index={true} element={<Navigate to="/job-board"/>} />
-
-  </Routes>
-</Router>
-
+    <BrowserRouter basename="/job-board">
+      <Routes>
+        <Route
+          path="/job-board"
+          element={!loggedIn ? <LoginPage setLoggedIn={handleLogin} /> : <Navigate to="/home" />}
+        />
+        <Route
+          path="/home"
+          element={loggedIn ? <HomePage onLogout={handleLogout} /> : <Navigate to="/job-board" />}
+        />
+        <Route
+          path="/employee"
+          element={loggedIn ? <Employee onJobSubmit={handleJobSubmit} /> : <Navigate to="/job-board" />}
+        />
+        <Route
+          path="/candidate"
+          element={loggedIn ? <Candidate onJobSubmit={handleJobSubmit} /> : <Navigate to="/job-board" />}
+        />
+  
+        <Route index={true} element={<Navigate to="/job-board" />} />
+      </Routes>
+    </BrowserRouter>
   );
+  
 }
 
 export default App;
