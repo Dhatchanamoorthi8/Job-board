a9c16926956504b3b1fe28f03eab4d47b7c2e6e2
SARUN <dhatchanamoorthiapcse19@jkkmct.edu.in>
2023-11-17 15:26:29 +0530

    first commit

--- src/components/Pages/HomePage.js
+++ src/components/Pages/HomePage.js
@@ -99,7 +99,7 @@ const [isDarkMode, setIsDarkMode] = useState(() => {
             <Link className="navbar-brand">
               <img src={require("../img/job-search (2).png")} alt="Bootstrap" width="50" height="50" />
             </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
